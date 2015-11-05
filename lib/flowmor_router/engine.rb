d14704333fb0cbe59b64b3e6025778b4d17b8e10
mwlang <mwlang@cybrains.net>
2015-11-05 14:31:21 -0800

    fixed initializer hook

--- lib/flowmor_router/engine.rb
+++ lib/flowmor_router/engine.rb
@@ -5,18 +5,19 @@ module FlowmorRouter
     # In development mode (i.e. eager_load == false), models are lazily loaded, 
     # so we hunt for them and explicitly require them to trigger the route redraws
     initializer :set_routes_reloader_hook do
-      return if Rails.configuration.eager_load
-
-      begin
-        models_path = File.join(Rails.root, 'app', 'models', '*')
-        routable_models = `grep -lr acts_as_routable #{models_path}`.split("\n")
-        routable_models.each do |fn|
-          Rails.logger.debug "PRELOADING: #{fn}"
-          require fn
+      if !Rails.configuration.eager_load
+        begin
+          models_path = File.join(Rails.root, 'app', 'models', '*')
+          routable_models = `grep -lr acts_as_routable #{models_path}`.split("\n")
+          routable_models.each do |fn|
+            Rails.logger.debug "PRELOADING: #{fn}"
+            require fn
+          end
+        rescue Exception => err
+          Rails.logger.error "FlowmorRouter: set_routes_reloader_hook could not pre-load routable models: #{err.message}"
         end
-      rescue Exception => err
-        Rails.logger.error "FlowmorRouter: set_routes_reloader_hook could not pre-load routable models: #{err.message}"
       end
     end
+    
   end
 end
